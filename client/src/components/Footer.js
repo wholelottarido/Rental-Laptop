a08d6ef14a81c9cbdcfd3225ae3b6732b38977fb
wholelottarido <eridhooo19@gmail.com>
2025-08-14 20:36:13 +0700

    fix title

--- client/src/components/Footer.js
+++ client/src/components/Footer.js
@@ -4,7 +4,7 @@ const Footer = () => {
   return (
     <footer className="bg-gray-800 text-white py-4 mt-auto">
       <div className="container mx-auto px-4 text-center">
-        <p>&copy; 2024 Rent-A-Top. All Rights Reserved.</p>
+        <p>&copy; 2025 Rent-A-Top. All Rights Reserved.</p>
       </div>
     </footer>
   );
